a288124c6fd2b612e940dff1cc395aa96a0385cc
vyong <vyong@udel.edu>
2018-12-22 02:16:47 -0500

    More unit testing

---
+++ UnitTesting/IncrementingResourcesTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameLib.IncrementingResources;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class IncrementingResourcesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IncrementingResource corn = new Corn();
+            corn.Value = 10;
+            corn.Delta = 5;
+
+            // Proc a tick
+            corn.Tick();
+            Assert.AreEqual(corn.Value, 15);
+            Assert.AreEqual(corn.Delta, 5);
+        }
+    }
+}
